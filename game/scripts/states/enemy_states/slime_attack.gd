1b1ea98a69254b0ae6f91405d92b6d3276712ad9
Marcus F <marcusf.dev@gmail.com>
2025-05-26 16:06:37 +0100

    Add: Player & Enemy attack scripts.

---
+++ game/scripts/states/enemy_states/slime_attack.gd
@@ -0,0 +1,26 @@
+class_name SlimeAttackState
+extends State
+
+@onready var timer: Timer = $attack_timer
+@export var slime_idle: SlimeIdleState
+var finished : bool = false
+
+# Called when the node enters the scene tree for the first time.
+func enter() -> void:
+	super.enter()
+	finished = false
+	print("Playing Slime Attack!")
+	animations.play("slime_attack")
+	
+	if not timer.is_connected("timeout", _on_attack_timer_timeout):
+		timer.connect("timeout", _on_attack_timer_timeout)
+	timer.start()
+
+func _on_attack_timer_timeout() -> void:
+	finished = true
+
+func process_frame(_delta: float) -> State:
+	if finished:
+		print("Slime has entered idle from attack.")
+		return slime_idle
+	return null
