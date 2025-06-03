64dd1fda38e3b596db07ea44a10d339c9d17d079
Marcus F <marcusf.dev@gmail.com>
2025-06-04 00:41:20 +0100

    Change: Hud adjusted to handle graphics only.

--- game/scripts/ui/hud.gd
+++ game/scripts/ui/hud.gd
@@ -1,8 +1,18 @@
+# hud.gd
+# 
+# This script is responsible for displaying and updating the user interface (UI) visuals,
+# such as score animations and labels. It does not contain or handle any game logic.
+# 
+# Dependencies:
+# - Expects 'game_manager.gd' to manage and emit changes.
+
 extends Control
 
-var last_health := 3  # Track the previous health
+@onready var hearts:= [$HealthBar/Heart1, $HealthBar/Heart2, $HealthBar/Heart3]
+@onready var coin_animation: AnimatedSprite2D = $CoinCounter/Coin
+@onready var score_label: Label = $CoinCounter/ScoreLabel
 
-@onready var hearts := [$HealthBar/Heart1, $HealthBar/Heart2, $HealthBar/Heart3]
 
-func _ready() -> void:
-	return
+func update_score(new_score: int) -> void:
+	coin_animation.play("gain_coin")
+	score_label.text = "x " + str(new_score)
