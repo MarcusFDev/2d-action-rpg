907bd6613a539bf515b41fd2905b06bebbfa5c36
Marcus F <marcusf.dev@gmail.com>
2025-06-04 00:03:18 +0100

    Add: Background & New Buttons to menu.

--- game/scripts/ui/main_menu.gd
+++ game/scripts/ui/main_menu.gd
@@ -3,12 +3,18 @@ extends Control
 
 
 func _on_play_btn_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/levels/game.tscn")
+	Engine.time_scale = 1
+	get_tree().change_scene_to_file("res://scenes/levels/developer_level.tscn")
 
 
 func _on_options_btn_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/ui/options_menu.tscn")
 
 
 func _on_quit_btn_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_tutorial_button_pressed() -> void:
+	Engine.time_scale = 1
+	get_tree().change_scene_to_file("res://scenes/levels/tutorial_level.tscn")
