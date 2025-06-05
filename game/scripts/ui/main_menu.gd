407bb6cf0babb9a017c3d2d30eeb6815a3a97784
Marcus F <marcusf.dev@gmail.com>
2025-06-05 10:41:00 +0100

    Change: Connected PlayButton to LevelPicker scene.

--- game/scripts/ui/main_menu.gd
+++ game/scripts/ui/main_menu.gd
@@ -3,14 +3,9 @@ extends Control
 
 
 func _on_play_btn_pressed() -> void:
-	Engine.time_scale = 1
-	get_tree().change_scene_to_file("res://scenes/levels/developer_level.tscn")
+	get_tree().change_scene_to_file("res://scenes/levels/level_picker.tscn")
 
-
-func _on_options_btn_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/ui/options_menu.tscn")
-
-
+	
 func _on_quit_btn_pressed() -> void:
 	get_tree().quit()
 
@@ -18,3 +13,7 @@ func _on_quit_btn_pressed() -> void:
 func _on_tutorial_button_pressed() -> void:
 	Engine.time_scale = 1
 	get_tree().change_scene_to_file("res://scenes/levels/tutorial_level.tscn")
+
+
+func _on_settings_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/ui/settings_menu.tscn")
