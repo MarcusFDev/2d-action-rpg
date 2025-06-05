2f3231535f6180f17228ed84d9a911285ab13906
Marcus F <marcusf.dev@gmail.com>
2025-06-05 21:03:33 +0100

    Fix: Removed old button signals.

--- game/scripts/ui/main_menu.gd
+++ game/scripts/ui/main_menu.gd
@@ -1,19 +1,17 @@
 extends Control
 
 
-
 func _on_play_btn_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/level_picker.tscn")
-
-	
-func _on_quit_btn_pressed() -> void:
-	get_tree().quit()
-
-
-func _on_tutorial_button_pressed() -> void:
-	Engine.time_scale = 1
-	get_tree().change_scene_to_file("res://scenes/levels/tutorial_level.tscn")
 
 
 func _on_settings_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/ui/settings_menu.tscn")
+
+
+func _on_credits_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/ui/credits_menu.tscn")
+
+
+func _on_quit_btn_pressed() -> void:
+	get_tree().quit()
