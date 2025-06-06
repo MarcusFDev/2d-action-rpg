c9c97ebfe6b9b55d20087117896d836f65dad975
Marcus F <marcusf.dev@gmail.com>
2025-06-06 15:40:31 +0100

    Fix: Solved bug causing GameOver scene timer incorrectly triggering.

--- game/scripts/states/player_states/player_death.gd
+++ game/scripts/states/player_states/player_death.gd
@@ -4,11 +4,10 @@ extends State
 
 func enter() -> void:
 	super()
-	
 	if parent is CharacterBody2D:
 		parent.velocity = Vector2.ZERO
-	
-	animations.animation_finished.connect(_on_animations_animation_finished, CONNECT_ONE_SHOT)
+	if not animations.animation_finished.is_connected(Callable(self, "_on_animations_animation_finished")):
+		animations.animation_finished.connect(_on_animations_animation_finished, CONNECT_ONE_SHOT)
 
 func process_input(_event: InputEvent) -> State:
 	return null
@@ -17,5 +16,6 @@ func process_physics(_delta: float) -> State:
 	return null
 
 func _on_animations_animation_finished() -> void:
-	if parent.has_method("trigger_game_over"):
-		parent.trigger_game_over()
+	if animations.animation == "death":
+		if parent.has_method("trigger_game_over"):
+			parent.trigger_game_over()
