ce3dd69c19a16ea7ad2e009857660d88aa1151da
Marcus F <marcusf.dev@gmail.com>
2025-06-19 13:02:13 +0100

    Add: More Hearts to HUD & health visualization fix.
    scripts/ui/hud.gdscripts/ui/hud.gdscripts/ui/hud.gd# Date:      Thu Jun 19 13:02:13 2025 +0100

--- game/scripts/ui/hud.gd
+++ game/scripts/ui/hud.gd
@@ -11,13 +11,27 @@ extends Control
 @onready var hearts: Array[AnimatedSprite2D] = [
 	$HealthBar/Heart1,
 	$HealthBar/Heart2,
-	$HealthBar/Heart3
+	$HealthBar/Heart3,
+	$HealthBar/Heart4,
+	$HealthBar/Heart5,
+	$HealthBar/Heart6,
+	$HealthBar/Heart7,
+	$HealthBar/Heart8,
+	$HealthBar/Heart9
 	]
 @onready var coin_animation: AnimatedSprite2D = $CoinCounter/Coin
 @onready var score_label: Label = $CoinCounter/ScoreLabel
 
 func _ready() -> void:
 	GameManager.hud = self
+	# Dynamically initialize hearts based on current player health
+	var player : CharacterBody2D = get_tree().get_nodes_in_group("player").front() as CharacterBody2D
+	if player:
+		_init_hearts(player.current_health)
+
+func _init_hearts(current_health: int) -> void:
+	for i: int in hearts.size():
+		hearts[i].visible = i < current_health
 
 func update_score(new_score: int) -> void:
 	coin_animation.play("gain_coin")
@@ -31,4 +45,5 @@ func update_health(old_health: int, new_health: int) -> void:
 	elif new_health > old_health:
 		for i: int in range(old_health, new_health):
 			if i >= 0 and i < hearts.size():
+				hearts[i].visible = true
 				hearts[i].play("gain_health")
