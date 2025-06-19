16f7fb55cb0399f285151fc4a314b829a374c71b
Marcus F <marcusf.dev@gmail.com>
2025-06-19 12:29:40 +0100

    Add: HeartCrystal sprite & script functionality.

--- game/scripts/core/heart_crystal.gd
+++ game/scripts/core/heart_crystal.gd
@@ -1,6 +1,10 @@
 extends Area2D
 
+@export var health_amount: int = 1
 var game_manager: Node = GameManager
 
 func _on_body_entered(body: CharacterBody2D) -> void:
-	game_manager.add_health()
+	if body.is_in_group("player"):
+		if body.current_health < body.max_health:
+			body.add_health(health_amount)
+			queue_free()
