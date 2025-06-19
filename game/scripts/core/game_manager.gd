24b1944ff5b159961141f6a4dd51d420fd37870c
Marcus F <marcusf.dev@gmail.com>
2025-06-19 13:13:07 +0100

    Fix: Line spacing within file.

--- game/scripts/core/game_manager.gd
+++ game/scripts/core/game_manager.gd
@@ -4,12 +4,10 @@ extends Node
 
 var score: int = 0
 
-
 func add_point() -> void:
 	score += 1
 	print("Your Score is: ", score)
 	hud.update_score(score)
-	
 
 func restart_level() -> void:
 	score = 0
