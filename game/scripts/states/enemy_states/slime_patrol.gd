d39d2a21271a9cf723307212660fb9f884f860bf
Marcus F <marcusf.dev@gmail.com>
2025-06-26 18:17:04 +0100

    Change: Commented out legacy move state scripts.

--- game/scripts/states/enemy_states/slime_patrol.gd
+++ game/scripts/states/enemy_states/slime_patrol.gd
@@ -1,59 +1,59 @@
 class_name SlimePatrolState
 extends State
-
-var direction : int = 1
-@export var speed : float = 60.0
-@export var slime_idle: IdleState
-@export var slime_fall: SlimeFallState
-
-var ray_cast_right: RayCast2D
-var ray_cast_left: RayCast2D
-
-var delay_timer : float = 0.15
-var idle_chance_timer : float = 0.0
-@export var idle_check_interval : float = 1.0
-@export var idle_chance : float = 0.2
-
-func enter() -> void:
-	super.enter()
-	delay_timer = 0.15
-	idle_chance_timer = 0.0
-	
-	ray_cast_right = parent.get_node("RayCastRight")
-	ray_cast_left = parent.get_node("RayCastLeft")
-
-func process_physics(delta: float) -> State:
-	if not parent.is_on_floor():
-		return slime_fall
-
-	parent.velocity.y += gravity * delta
-	parent.velocity.x = direction * speed
-	return null
-
-func process_frame(delta: float) -> State:
-	if delay_timer > 0.0:
-		delay_timer -= delta
-	else:
-		if ray_cast_right.is_colliding():
-			direction = -1
-			animations.flip_h = true
-			slime_idle.set_direction(direction)
-			#print("Slime collided right, going idle.")
-			return slime_idle
-
-		elif ray_cast_left.is_colliding():
-			direction = 1
-			animations.flip_h = false
-			slime_idle.set_direction(direction)
-			#print("Slime collided left, going idle.")
-			return slime_idle
-	
-	idle_chance_timer += delta
-	if idle_chance_timer >= idle_check_interval:
-		idle_chance_timer = 0.0
-		if randf() < idle_chance:
-			slime_idle.set_direction()
-			#print("Slime randomly began to idle.")
-			return slime_idle
-	
-	return null
+#
+#var direction : int = 1
+#@export var speed : float = 60.0
+#@export var slime_idle: IdleState
+#@export var slime_fall: SlimeFallState
+#
+#var ray_cast_right: RayCast2D
+#var ray_cast_left: RayCast2D
+#
+#var delay_timer : float = 0.15
+#var idle_chance_timer : float = 0.0
+#@export var idle_check_interval : float = 1.0
+#@export var idle_chance : float = 0.2
+#
+#func enter() -> void:
+	#super.enter()
+	#delay_timer = 0.15
+	#idle_chance_timer = 0.0
+	#
+	#ray_cast_right = parent.get_node("RayCastRight")
+	#ray_cast_left = parent.get_node("RayCastLeft")
+#
+#func process_physics(delta: float) -> State:
+	#if not parent.is_on_floor():
+		#return slime_fall
+#
+	#parent.velocity.y += gravity * delta
+	#parent.velocity.x = direction * speed
+	#return null
+#
+#func process_frame(delta: float) -> State:
+	#if delay_timer > 0.0:
+		#delay_timer -= delta
+	#else:
+		#if ray_cast_right.is_colliding():
+			#direction = -1
+			#animations.flip_h = true
+			#slime_idle.set_direction(direction)
+			##print("Slime collided right, going idle.")
+			#return slime_idle
+#
+		#elif ray_cast_left.is_colliding():
+			#direction = 1
+			#animations.flip_h = false
+			#slime_idle.set_direction(direction)
+			##print("Slime collided left, going idle.")
+			#return slime_idle
+	#
+	#idle_chance_timer += delta
+	#if idle_chance_timer >= idle_check_interval:
+		#idle_chance_timer = 0.0
+		#if randf() < idle_chance:
+			#slime_idle.set_direction()
+			##print("Slime randomly began to idle.")
+			#return slime_idle
+	#
+	#return null
--- game/scripts/states/player_states/player_move.gd
+++ game/scripts/states/player_states/player_move.gd
@@ -1,33 +1,33 @@
 class_name PlayerMoveState
 extends State
-
-@export var jump: State
-@export var fall: State
-@export var idle: State
-@export var jump_force: float = -300
-
-func process_physics(delta: float) -> State:
-	parent.velocity.y += gravity * delta
-	
-	var input_direction: float = Input.get_axis("move_left", "move_right")
-	var movement: float = input_direction * move_speed
-	
-	if movement == 0:
-		#print("Idle triggered from move.gd")
-		return idle
-	
-	parent.animations.flip_h = movement < 0
-	parent.velocity.x = movement
-	
-	if Input.is_action_just_pressed("jump") and parent.is_on_floor():
-		parent.velocity.y = jump_force
-		#print("Jump triggered from move.gd")
-		return jump
-	
-	parent.move_and_slide()
-	
-	if not parent.is_on_floor():
-		#print("fall triggered from move.gd")
-		return fall
-	
-	return null
+#
+#@export var jump: State
+#@export var fall: State
+#@export var idle: State
+#@export var jump_force: float = -300
+#
+#func process_physics(delta: float) -> State:
+	#parent.velocity.y += gravity * delta
+	#
+	#var input_direction: float = Input.get_axis("move_left", "move_right")
+	#var movement: float = input_direction * move_speed
+	#
+	#if movement == 0:
+		##print("Idle triggered from move.gd")
+		#return idle
+	#
+	#parent.animations.flip_h = movement < 0
+	#parent.velocity.x = movement
+	#
+	#if Input.is_action_just_pressed("jump") and parent.is_on_floor():
+		#parent.velocity.y = jump_force
+		##print("Jump triggered from move.gd")
+		#return jump
+	#
+	#parent.move_and_slide()
+	#
+	#if not parent.is_on_floor():
+		##print("fall triggered from move.gd")
+		#return fall
+	#
+	#return null
