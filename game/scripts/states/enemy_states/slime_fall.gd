5ada1931d8de59c22899902bec4a01559d34ab81
Marcus F <marcusf.dev@gmail.com>
2025-07-02 14:07:03 +0100

    Change: Commented out legacy fall scripts.

--- game/scripts/states/enemy_states/slime_fall.gd
+++ game/scripts/states/enemy_states/slime_fall.gd
@@ -1,17 +1,17 @@
 class_name SlimeFallState
 extends State
 
-@export var slime_patrol: MoveState
-
-func enter() -> void:
-	super.enter()
-	animations.play("slime_fall")
-	
-func process_physics(delta: float) -> State:
-	parent.velocity.y += gravity * delta
-	
-	if parent.is_on_floor():
-		#print("Slime has stopped falling, going on patrol")
-		return slime_patrol
-	
-	return null
+#@export var slime_patrol: MoveState
+#
+#func enter() -> void:
+	#super.enter()
+	#animations.play("slime_fall")
+	#
+#func process_physics(delta: float) -> State:
+	#parent.velocity.y += gravity * delta
+	#
+	#if parent.is_on_floor():
+		##print("Slime has stopped falling, going on patrol")
+		#return slime_patrol
+	#
+	#return null
