1dcbe684bbf34b8e9d4cad1f09e60039b0984653
Marcus F <marcusf.dev@gmail.com>
2025-07-02 18:23:44 +0100

    Change: Commented out legacy death scripts.

--- game/scripts/states/player_states/player_death.gd
+++ game/scripts/states/player_states/player_death.gd
@@ -2,20 +2,20 @@ class_name PlayerDeathState
 extends State
 
 
-func enter() -> void:
-	super()
-	if parent is CharacterBody2D:
-		parent.velocity = Vector2.ZERO
-	if not animations.animation_finished.is_connected(Callable(self, "_on_animations_animation_finished")):
-		animations.animation_finished.connect(_on_animations_animation_finished, CONNECT_ONE_SHOT)
-
-func process_input(_event: InputEvent) -> State:
-	return null
-
-func process_physics(_delta: float) -> State:
-	return null
-
-func _on_animations_animation_finished() -> void:
-	if animations.animation == "death":
-		if parent.has_method("trigger_game_over"):
-			parent.trigger_game_over()
+#func enter() -> void:
+	#super()
+	#if parent is CharacterBody2D:
+		#parent.velocity = Vector2.ZERO
+	#if not animations.animation_finished.is_connected(Callable(self, "_on_animations_animation_finished")):
+		#animations.animation_finished.connect(_on_animations_animation_finished, CONNECT_ONE_SHOT)
+#
+#func process_input(_event: InputEvent) -> State:
+	#return null
+#
+#func process_physics(_delta: float) -> State:
+	#return null
+#
+#func _on_animations_animation_finished() -> void:
+	#if animations.animation == "death":
+		#if parent.has_method("trigger_game_over"):
+			#parent.trigger_game_over()
