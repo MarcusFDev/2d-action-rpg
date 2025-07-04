63ba4ff27bd55acf9aeeb38b6191f35a147d7c45
Marcus F <marcusf.dev@gmail.com>
2025-07-04 12:43:13 +0100

    Change: Commented out legacy attack scripts.

--- game/scripts/states/enemy_states/slime_attack.gd
+++ game/scripts/states/enemy_states/slime_attack.gd
@@ -1,24 +1,24 @@
 class_name SlimeAttackState
 extends State
 
-@onready var timer: Timer = $AttackTimer
-@export var slime_idle: IdleState
-var finished : bool = false
-
-# Called when the node enters the scene tree for the first time.
-func enter() -> void:
-	super.enter()
-	finished = false
-	animations.play("slime_attack")
-	
-	if not timer.is_connected("timeout", _on_attack_timer_timeout):
-		timer.connect("timeout", _on_attack_timer_timeout)
-	timer.start()
-
-func _on_attack_timer_timeout() -> void:
-	finished = true
-
-func process_frame(_delta: float) -> State:
-	if finished:
-		return slime_idle
-	return null
+#@onready var timer: Timer = $AttackTimer
+#@export var slime_idle: IdleState
+#var finished : bool = false
+#
+## Called when the node enters the scene tree for the first time.
+#func enter() -> void:
+	#super.enter()
+	#finished = false
+	#animations.play("slime_attack")
+	#
+	#if not timer.is_connected("timeout", _on_attack_timer_timeout):
+		#timer.connect("timeout", _on_attack_timer_timeout)
+	#timer.start()
+#
+#func _on_attack_timer_timeout() -> void:
+	#finished = true
+#
+#func process_frame(_delta: float) -> State:
+	#if finished:
+		#return slime_idle
+	#return null
