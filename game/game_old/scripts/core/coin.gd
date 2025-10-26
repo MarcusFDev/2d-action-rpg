d50859bc482f385c1922bedb273ed053ebdb6029
Marcus F <marcusf.dev@gmail.com>
2025-10-26 11:27:36 +0000

    Change: Removed outdated v0.1 project files.

--- game/game_old/scripts/core/coin.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-var game_manager: Node = GameManager
-
-func _on_body_entered(_body: CharacterBody2D) -> void:
-	game_manager.add_point()
-	animation_player.play("pickup") # Function that removes coin upon player collision.
