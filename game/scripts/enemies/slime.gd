d0d00309a05f35c8c4fd184f6d3785947cf24af7
Marcus F <marcusf.dev@gmail.com>
2025-10-26 14:10:34 +0000

    Change: Integrated new BehaviorTree logic to Slime entity.

--- game/scripts/enemies/slime.gd
+++ game/scripts/enemies/slime.gd
@@ -1,5 +1,11 @@
 extends CharacterBody2D
 
+
+@export var damage: int = 1
+## Enables debug messages in the output terminal. [br]
+## [b]Note:[/b] Useful for development and troubleshooting.
+@export var enable_debug: bool = false
+
 @onready var animations: AnimatedSprite2D = $Animations
 @onready var state_machine: Node = $StateMachine
 @onready var idle_state: IdleState = $StateMachine/IdleState
@@ -8,18 +14,37 @@ extends CharacterBody2D
 @onready var fall_state: FallState = $StateMachine/FallState
 @onready var attack_state: AttackState = $StateMachine/AttackState
 
-@export var damage: int = 1
+# Components
+@onready var ground_check_component: Node = $Components/GroundCheckComponent
 
 var patrol_direction: int = 1
 
+var blackboard: Dictionary = {
+	"fsm": null,
+	"is_grounded": false,
+	"can_patrol": true,
+	"can_idle": true,
+	"intent": "Idle"
+}
+
+var bt_root: BTNode
+var bt: BehaviorTree
+
 func _ready() -> void:
 	_setup_states()
-	state_machine.init(self, animations)
-	call_deferred("_post_ready_check")
+	_setup_blackboard()
+	state_machine.init(self, animations, blackboard)
+	_setup_behavior_tree()
 
-func _post_ready_check() -> void:
-	if not is_on_floor():
-		state_machine.change_state(fall_state)
+func get_blackboard() -> Dictionary:
+	return blackboard
+
+func _setup_blackboard() -> void:
+	blackboard["fsm"] = state_machine
+	blackboard["is_grounded"] = false
+	blackboard["can_patrol"] = true
+	blackboard["can_idle"] = true
+	blackboard["intent"] = "Idle"
 
 func _setup_states() -> void:
 	_idle_state()
@@ -42,12 +67,43 @@ func _jump_state() -> void:
 func _fall_state() -> void:
 	pass
 
+func _setup_behavior_tree() -> void:
+	bt_root = BTSelector.new([
+		# Priority 1: Fall if not grounded
+		BTSequence.new([
+			BTCondition.new("is_grounded", false),
+			BTAction.new("Fall")
+		]),
+		# Priority 2: Patrol if grounded and allowed
+		BTSequence.new([
+			BTCondition.new("is_grounded", true),
+			BTCondition.new("can_patrol", true),
+			BTAction.new("Patrol")
+		]),
+		# Priority 3: Idle if grounded and allowed
+		BTSequence.new([
+			BTCondition.new("is_grounded", true),
+			BTCondition.new("can_idle", true),
+			BTAction.new("Idle")
+		])
+	])
+	bt = BehaviorTree.new(bt_root, blackboard, self)
+
 func _process(delta: float) -> void:
+	if bt:
+		bt.tick()
 	state_machine.process_frame(delta)
+	if enable_debug:
+		print(
+		"Intent:", blackboard["intent"],
+		" | Grounded:", blackboard["is_grounded"],
+		" | State:", state_machine.current_state.name
+	)
 
 func _physics_process(delta: float) -> void:
 	state_machine.process_physics(delta)
 	move_and_slide()
+	ground_check_component.apply(delta)
 
 func trigger_attack() -> void:
 	if attack_state:
