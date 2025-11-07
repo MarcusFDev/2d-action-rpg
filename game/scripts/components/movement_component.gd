b30b83775cd9c17388e663462d891b819441c804
Marcus F <marcusf.dev@gmail.com>
2025-11-07 23:53:24 +0000

    Add: 'randomize_direction' function to MocementComponent.

--- game/scripts/components/movement_component.gd
+++ game/scripts/components/movement_component.gd
@@ -12,10 +12,21 @@ extends Node
 ## [b]Note:[/b] Can exceed 200 with manual input.
 @export_range(0, 200, 1, "suffix:px/s", "or_greater") var move_speed: float = 0
 
+@export_group("Randomize Direction")
+@export_range(0, 100, 1, "suffix:%") var randomized_chance: float
+
 @onready var actor: CharacterBody2D = get_node_or_null(actor_path)
 
 # Script Variables
 var direction: Vector2 = Vector2.ZERO
+
+func randomize_direction(direction: int) -> int:
+	var flip_chance: float = randomized_chance / 100.0
+
+	if randf() < flip_chance:
+		direction *= -1
+
+	return direction
 
 func set_direction(new_direction: Vector2) -> void:
 	direction = new_direction.normalized()
