e778bbd2918dbbc80ae9ec60dcc66108e84fff62
Marcus F <marcusf.dev@gmail.com>
2025-11-07 23:44:16 +0000

    Add: Complete JumpComponent intergration with JumpState.

--- game/scripts/states/generic_states/jump_state.gd
+++ game/scripts/states/generic_states/jump_state.gd
@@ -1,18 +1,33 @@
 class_name JumpState
 extends State
 
-## Determines the animation that plays during the Jump State.[br]
-## [b]Note:[/b] Must match an animation name in  [code]AnimatedSprite2D[/code]  or  [code]AnimationPlayer[/code].
-@export var jump_animation: String
-
-@export var use_behavior_tree: bool = false
-@export var use_parent_logic: bool = false
+@export var actor_path: NodePath
 ## Enables debug messages in the output terminal. [br]
 ## [b]Note:[/b] Useful for development and troubleshooting.
 @export var enable_debug: bool = false
 
-@onready var gravity_component: Node = $"../../Components/GravityComponent"
-@onready var jump_component: Node = $"../../Components/JumpComponent"
+@export_category("State Settings")
+## Determines the animation that plays during the Jump State.[br]
+## [b]Note:[/b] Must match an animation name in  [code]AnimatedSprite2D[/code]  or  [code]AnimationPlayer[/code].
+@export var state_animation: String
+@export var use_parent_logic: bool = false
+
+@export_group("Use Behavior Tree", "export_")
+@export_custom(PROPERTY_HINT_GROUP_ENABLE, "") var export_use_behavior_tree: bool = false
+@export var export_enable_randomize_direction: bool = false
+
+@export_group("Component Paths")
+
+@export var gravity_component: NodePath
+@export var ground_check_component: NodePath
+@export var jump_component: NodePath
+@export var movement_component: NodePath
+
+@onready var actor: CharacterBody2D = get_node_or_null(actor_path)
+@onready var ground_check_comp: Node = get_node_or_null(ground_check_component)
+@onready var jump_comp: Node = get_node_or_null(jump_component)
+@onready var gravity_comp: Node = get_node_or_null(gravity_component)
+@onready var movement_comp: Node = get_node_or_null(movement_component)
 
 # Callback Functions
 func _on_enter() -> void: pass
@@ -26,35 +41,86 @@ var handle_input: Callable = _on_input
 var handle_physics: Callable = _on_physics
 var handle_frame: Callable = _on_frame
 
+# Script Variables
 var direction: int = 0
+var jumps_remaining: int = 0
+var jump_timer: float = 0.0
+var jump_cooldown: bool = false
 
+# --------------------------------------------------------------------
+# STATE ENTER
+# --------------------------------------------------------------------
 func enter() -> void:
 	super.enter()
-	init_jump()
-
-func init_jump() -> void:
-	parent.animations.play(jump_animation)
-	if use_parent_logic:
-		jump_component.start_jump()
 	if enable_debug:
 		print(
 		"===========================\n",
-		"jumpstate entered by: ", parent.name, "\n",
+		"JumpState entered by: ", actor.name, "\n",
 		"===========================")
+	init_jump()
 
-func process_physics(delta: float) -> State:
-	if use_behavior_tree:
-		var bb: Dictionary = parent.get_blackboard()
-		direction = bb["move_direction"]
-		var jump_target: Variant = parent.global_position + Vector2(64 * direction, -32)
-		jump_component.jump_to(jump_target)
-	
-	parent.move_and_slide()
+func init_jump() -> void:
+	actor.animations.play(state_animation)
 	
 	if use_parent_logic:
-		jump_component.apply(delta)
-		return handle_physics.call(delta)
+		jump_comp.perform_jump()
 	
+	if export_use_behavior_tree:
+		var bb: Dictionary = actor.get_blackboard()
+		bb["locked"] = true
+		bb["is_jumping"] = true
+		bb["can_jump"] = false
+		bb["has_collided"] = false
+		direction = bb["move_direction"]
+
+		if export_enable_randomize_direction:
+			bb["direction"] = movement_comp.randomize_direction(direction)
+		else:
+			direction = bb["move_direction"]
+		
+		jumps_remaining = jump_comp.base_jumps + jump_comp.initial_extra_jumps
+
+		if enable_debug:
+			print(
+				actor.name,
+				" | JumpState: Combining Base Jumps: ", jump_comp.base_jumps,
+				" & Extra Jumps: ", jump_comp.initial_extra_jumps,
+				" to: ", jumps_remaining, " jumps.")
+
+# --------------------------------------------------------------------
+# STATE PHYSICS
+# --------------------------------------------------------------------
+func process_physics(delta: float) -> State:
+	actor.move_and_slide()
+	jump_comp.apply_physics(delta)
+
+	if export_use_behavior_tree:
+		var bb: Dictionary = actor.get_blackboard()
+		var grounded: bool = bb["is_grounded"]
+		
+		if grounded:
+			if not jump_comp.cooldown_active and jumps_remaining > 0:
+				jump_comp.perform_target_jump(direction)
+				jump_comp.start_cooldown()
+				
+				jumps_remaining -= 1
+				
+			elif jump_comp.cooldown_active:
+				actor.velocity.x = 0
+				pass
+			
+			else:
+				jump_comp.reset_jump_counter()
+				bb["can_jump"] = true
+				bb["is_jumping"] = false
+				bb["locked"] = false
+				bb["can_patrol"] = true
+
+				if enable_debug:
+					print(actor.name, " | JumpState finished all jumps.")
+
+	if use_parent_logic:
+		return handle_physics.call(delta)
 	return null
 
 func process_input(event: InputEvent) -> State:
@@ -68,4 +134,5 @@ func process_frame(delta: float) -> State:
 	return null
 
 func exit() -> void:
-	pass
+	if enable_debug:
+		print(actor.name, " | JumpState exited.")
