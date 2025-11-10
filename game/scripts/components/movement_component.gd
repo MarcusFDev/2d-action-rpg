75747c0fbe8d33bd6c256c05a87f69c3ec4b3cd4
Marcus F <marcusf.dev@gmail.com>
2025-11-10 02:10:47 +0000

    Change: Rewrote 'direction_randomizer' function to take values.

--- game/scripts/components/movement_component.gd
+++ game/scripts/components/movement_component.gd
@@ -12,26 +12,41 @@ extends Node
 ## [b]Note:[/b] Can exceed 200 with manual input.
 @export_range(0, 200, 1, "suffix:px/s", "or_greater") var move_speed: float = 0
 
-@export_group("Randomize Direction")
-@export_range(0, 100, 1, "suffix:%") var randomized_chance: float
+@export_group("Direction Options")
+
 @export var use_vector_direction: bool = false
 
 @onready var actor: CharacterBody2D = get_node_or_null(actor_path)
 
 # Script Variables
 var direction: Vector2 = Vector2.ZERO
+var _timers : Dictionary = {}
 
-func randomize_direction() -> Vector2:
-	var flip_chance: float = randomized_chance / 100.0
-
-	if randf() < flip_chance:
-		if use_vector_direction:
-			direction = Vector2(randf_range(-1.0, 1.0), randf_range(-1.0, 1.0)).normalized()
-		else:
-			direction.x *= -1.0
+func direction_randomizer(id: String, chance: float, interval: float, delta: float) -> Variant:
+	if interval <= 0:
+		if enable_debug:
+			print(actor.name, " MovementComponent: DirectionRandomizer entered but interval value 0.")
+		return {"success": false, "direction": direction}
 	
-	return direction
-
+	if not _timers.has(id):
+		_timers[id] = 0.0
+	
+	_timers[id] += delta
+	if _timers[id] >= interval:
+		_timers[id] = 0.0
+		var roll: float = randf() * 100.0
+		var success: bool = roll < chance
+		if success: 
+			if use_vector_direction:
+				direction = Vector2(randf_range(-1.0, 1.0), randf_range(-1.0, 1.0)).normalized()
+			else:
+				direction.x *= -1.0
+			if enable_debug:
+				print(actor.name, " | MovementComponent: DirectionRandomizer: Id:", id, " | Roll Value:", roll, " | Chance: ", chance," | Direction:", direction, " | Success: ", success)	
+				
+			return {"success": true, "direction": direction}
+	
+	return {"success": false, "direction": direction}
 
 func set_direction(new_direction: Vector2) -> void:
 	direction = new_direction.normalized()
