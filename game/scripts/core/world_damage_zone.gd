ddabf1e93586eff3ae4650da9be6f8cfb2615f0d
Marcus F <marcusf.dev@gmail.com>
2025-11-16 23:00:45 +0000

    Change: Disabled all Damage zones for rework later.

--- game/scripts/core/world_damage_zone.gd
+++ game/scripts/core/world_damage_zone.gd
@@ -1,5 +1,5 @@
 extends BaseDamageZone
-
-func apply_damage(body: Node) -> void:
-	body.take_damage(9999, global_position)
-	print("WorldKillZone | Player instantly killed")
+#
+#func apply_damage(body: Node) -> void:
+	#body.take_damage(9999, global_position)
+	#print("WorldKillZone | Player instantly killed")
