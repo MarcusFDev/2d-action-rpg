fb4ba1a9b74c4dc2ef98239020d12db68aeabb99
Marcus F <marcusf.dev@gmail.com>
2025-11-19 20:13:45 +0000

    Add: Full functionality to HealState handling healing.

--- game/scripts/states/generic_states/heal_state.gd
+++ game/scripts/states/generic_states/heal_state.gd
@@ -16,8 +16,12 @@ extends State
 @export var use_parent_logic: bool = false
 @export var use_behavior_tree: bool = false
 
+@export_group("Component Paths")
+@export var health_component_path: NodePath
+
 # Script Variables
 @onready var actor: Node = get_node_or_null(actor_path)
+@onready var health_comp: Node = get_node_or_null(health_component_path)
 
 # Callback Functions
 func _on_enter() -> void: pass
@@ -31,22 +35,44 @@ var handle_input: Callable = _on_input
 var handle_physics: Callable = _on_physics
 var handle_frame: Callable = _on_frame
 
+var animation_finished: bool
+var heal_amount: float = 0.0
+
 func enter() -> void:
 	super.enter()
-	init_heal()
-
-func init_heal() -> void:
-	actor.animations.play(state_animation)
-
 	if enable_debug:
 		print(
 		"===========================\n",
 		"HealState entered by: ", actor.name, "\n",
 		"===========================")
+	
+	init_heal()
+
+func init_heal() -> void:
+	actor.animations.play(state_animation)
+	animation_finished = false
+	
+	if use_behavior_tree:
+		var bb: Dictionary = actor.get_blackboard()
+		bb["locked"] = true
 
 func process_physics(delta: float) -> State:
-	if use_parent_logic:
-		return handle_physics.call(delta)
+	actor.velocity.x = 0
+	
+	if animation_finished:
+		if health_comp:
+			if enable_debug:
+				print(actor.name, " | HealState: Calling HealthComponent with heal_value: ", heal_amount)
+			health_comp.gain_health(heal_amount)
+		
+		if use_behavior_tree:
+			var bb: Dictionary = actor.get_blackboard()
+			bb["force_heal"] = false
+			bb["locked"] = false
+		
+		if use_parent_logic:
+			return handle_physics.call(delta)
+	
 	return null
 
 func process_frame(delta: float) -> State:
@@ -56,5 +82,19 @@ func process_frame(delta: float) -> State:
 
 func process_input(event: InputEvent) -> State:
 	if use_parent_logic:
-		return handle_input.call(event)
+		if animation_finished:
+			return handle_input.call(event)
 	return null
+
+func set_heal_data(data: Dictionary) -> void:
+	heal_amount = data["amount"]
+
+func on_animation_finished() -> void:
+	if actor.animations.animation == state_animation:
+		if enable_debug:
+			print(actor.name, " | HealState: Animation finished.")
+		animation_finished = true
+
+func exit() -> void:
+	if enable_debug:
+		print(actor.name, " | HealState exited.")
