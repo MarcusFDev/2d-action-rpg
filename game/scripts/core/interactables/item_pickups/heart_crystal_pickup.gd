24d0da5eae8b185bc91251bd11b7eb65ffbd6f2f
Marcus F <marcusf.dev@gmail.com>
2025-11-29 22:36:55 +0000

    Change: Renamed Dictionary to 'item_data'.

--- game/scripts/core/interactables/item_pickups/heart_crystal_pickup.gd
+++ game/scripts/core/interactables/item_pickups/heart_crystal_pickup.gd
@@ -6,7 +6,7 @@ extends ItemPickUp
 @export_range(0, 10, 1, "suffix:Hearts", "or_greater") var heal_amount: float
 
 func _enter_tree() -> void:
-	pickup_data = {
+	item_data = {
 		"type": "heal",
 		"source": "heart_crystal",
 		"amount": heal_amount
--- game/scripts/core/interactables/item_pickups/item_pickup.gd
+++ game/scripts/core/interactables/item_pickups/item_pickup.gd
@@ -1,7 +1,7 @@
 class_name ItemPickUp
 extends BaseInteractables
 
-var pickup_data: Dictionary = {}
+var item_data: Dictionary = {}
 
 func _ready() -> void:
 	connect("actor_can_interact", on_pickup)
@@ -9,9 +9,9 @@ func _ready() -> void:
 func on_pickup(actor: Node) -> void:
 	var perm_comp: Node = actor.get("pickup_permission_comp")
 	if perm_comp:
-		actor.pickup_permission_comp.apply_pickup(pickup_data)
+		actor.pickup_permission_comp.apply_pickup(item_data)
 	elif actor.has_method("pickup_received"):
-		actor.pickup_received(pickup_data)
+		actor.pickup_received(item_data)
 	else:
 		if enable_debug:
 			print(actor.name, " has no PickUpPermissionComponent and no pickup_received() method.")
