ee9106774099bb7e93359adab8f1146c364c32c0
Marcus F <marcusf.dev@gmail.com>
2025-11-29 23:21:58 +0000

    Add: Coin collecting permissions to Component.

--- game/scripts/components/pickup_permission_component.gd
+++ game/scripts/components/pickup_permission_component.gd
@@ -12,10 +12,15 @@ signal pickup_received(data: Dictionary)
 @export_category("Component Settings")
 ## Determines whether this entity is allowed to receive healing pickups. [br]
 ## [b]Tip:[/b] Disable this for actors that should ignore health-restoring items.
-@export_group("Enable Health Pickup", "export_")
-@export_custom(PROPERTY_HINT_GROUP_ENABLE, "") var export_enable_health_pickup: bool = false
+@export_group("Enable Health Pickup", "export_hp_")
+@export_custom(PROPERTY_HINT_GROUP_ENABLE, "") var export_hp_enable_health_pickup: bool = false
+@export var export_hp_allow_heart_crystals: bool = false
 
-@export var export_allow_heart_crystals: bool = false
+## Determines whether this entity is allowed to receive healing pickups. [br]
+## [b]Tip:[/b] Disable this for actors that should ignore health-restoring items.
+@export_group("Enable Currency Pickup", "export_cur_")
+@export_custom(PROPERTY_HINT_GROUP_ENABLE, "") var export_cur_enable_currency_pickup: bool = false
+@export var export_cur_allow_coins: bool = false
 
 @onready var actor: Node = get_node_or_null(actor_path)
 
@@ -31,10 +36,18 @@ func apply_pickup(data: Dictionary) -> void:
 	
 	match data["type"]:
 		"heal":
-			if export_enable_health_pickup:
+			if export_hp_enable_health_pickup:
 				match data["source"]:
 					"heart_crystal":
-						if export_allow_heart_crystals:
+						if export_hp_allow_heart_crystals:
 							if enable_debug:
 								print(actor.name, " PickUpPermissionsComponent: HeartCrystal Picked Up.")
+							emit_signal("pickup_received", data)
+		"currency":
+			if export_cur_enable_currency_pickup:
+				match data["source"]:
+					"coin":
+						if export_cur_allow_coins:
+							if enable_debug:
+								print(actor.name, " PickUpPermissionsComponent: Coin Picked Up.")
 							emit_signal("pickup_received", data)
