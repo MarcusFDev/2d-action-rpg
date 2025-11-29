18248a711ab5cfa03ed37a47ec3aa123bfe3a358
Marcus F <marcusf.dev@gmail.com>
2025-11-29 00:16:08 +0000

    Change: Removed signal and instead call HurtBoxComponent a function.

--- game/scripts/components/hitbox_component.gd
+++ game/scripts/components/hitbox_component.gd
@@ -1,15 +1,11 @@
 class_name HitBoxComponent
 extends Area2D
 
-signal hitbox_entered(hitbox_owner: Node, hurtbox_owner: Node)
-
 ## Assign the parent entity to the script.
 @export var owner_path: NodePath
 ## Enables debug messages in the output terminal. [br]
 ## [b]Note:[/b] Useful for development and troubleshooting.
 @export var enable_debug: bool = false
-
-@export_category("Component Settings")
 
 @onready var hitbox_owner: Node = get_node_or_null(owner_path)
 
@@ -23,7 +19,10 @@ func on_area_entered(area: Area2D) -> void:
 		if hurtbox.hurtbox_owner == hitbox_owner:
 			return
 		
-		emit_signal("hitbox_entered", hitbox_owner, hurtbox.hurtbox_owner)
-			
-		if enable_debug:
-			print(hurtbox.hurtbox_owner.name, " | HitBoxComponent: Actor been detected by ", hitbox_owner.name)
+		if hitbox_owner.damage:
+			var hitbox_data: Variant = hitbox_owner.damage
+			hurtbox.received_hit(hitbox_owner, hitbox_data)
+
+			if enable_debug:
+				print(hitbox_owner.name, " | HitBoxComponent: ", hurtbox.hurtbox_owner.name, "'s HurtBox has been detected.")
+				print(hitbox_owner.name, " | HitBoxComponent: ", hitbox_owner.name, "'s HitBoxData: ", hitbox_data)
