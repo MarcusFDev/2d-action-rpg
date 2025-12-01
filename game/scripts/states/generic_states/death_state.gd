7619f0290369668bfe2c11d0be867a4084b70882
Marcus F <marcusf.dev@gmail.com>
2025-12-01 00:43:45 +0000

    Change: DeathState refactored to prepare for future gameover handling.

--- game/scripts/states/generic_states/death_state.gd
+++ game/scripts/states/generic_states/death_state.gd
@@ -1,18 +1,23 @@
 class_name DeathState
 extends State
 
-## Determines whether this state uses its own internal logic or defers to parent-defined callbacks.[br]
-## [b]Note:[/b] If set to [code]false[/code], all internal logic settings are ignored.
-@export var use_internal_logic: bool = true
-## Determines the animation that plays during the Death State.[br]
-## [b]Note:[/b] Must match an animation name in  [code]AnimatedSprite2D[/code]  or  [code]AnimationPlayer[/code].
-@export var death_animation : String
-## Determines the post-death  [code]method[/code]  the parent class uses to handle what happens when animation finishes.[br]
-## [b]Note:[/b] Must match method name exactly or unintended behavior may occur.
-@export var post_death_method: String
+## Assign the parent entity to the state.
+@export var actor_path: NodePath
 ## Enables debug messages in the output terminal. [br]
 ## [b]Note:[/b] Useful for development and troubleshooting.
 @export var enable_debug: bool = false
+
+@export_category("State Settings")
+## Determines the animation that plays during the Death State.[br]
+## [b]Note:[/b] Must match an animation name in  [code]AnimatedSprite2D[/code]  or  [code]AnimationPlayer[/code].
+@export var state_animation : String
+## Determines whether this state uses its own internal logic or defers to parent-defined callbacks.[br]
+## [b]Note:[/b] If set to [code]false[/code], all internal logic settings are ignored.
+@export var use_parent_logic: bool = false
+@export var use_behavior_tree: bool = false
+
+# Script Variables
+@onready var actor: Node = get_node_or_null(actor_path)
 
 # Callback Functions
 func _on_enter() -> void: pass
@@ -26,41 +31,36 @@ var handle_input: Callable = _on_input
 var handle_physics: Callable = _on_physics
 var handle_frame: Callable = _on_frame
 
+var animation_finished: bool
+
 func enter() -> void:
 	super.enter()
+	if enable_debug:
+		print(
+		"===========================\n",
+		"DeathState entered by: ", actor.name, "\n",
+		"===========================")
+	
 	init_death()
 
 func init_death() -> void:
-	parent.animations.play(death_animation)
+	actor.animations.play(state_animation)
+	animation_finished = false
 	
-	if use_internal_logic:
-		parent.velocity = Vector2.ZERO
-		if not animations.animation_finished.is_connected(animation_finished):
-			animations.animation_finished.connect(animation_finished, CONNECT_ONE_SHOT)
-
-	if enable_debug:
-		print(
-			"\n===== Death State: ===== \n", parent,
-			"\nAnimation playing: ", death_animation,
-			"\nVelocity set to: ", parent.velocity)
-	else:
-		enter_callback.call()
+	if use_behavior_tree:
+		var bb: Dictionary = actor.get_blackboard()
+		bb["locked"] = true
 
 func process_physics(_delta: float) -> State:
+	actor.velocity.x = 0
 	return null
 
-func animation_finished() -> void:
-	if animations.animation == death_animation:
+func on_animation_finished() -> void:
+	if actor.animations.animation == state_animation:
 		if enable_debug:
-			print("Death Animation finished.")
-
-		if post_death_method != "" and parent.has_method(post_death_method):
-			if enable_debug:
-				print("Post Death method set to: ", post_death_method)
-			parent.call(post_death_method)
-		else:
-			if enable_debug:
-				print(post_death_method, " method either does not exist in parent.")
+			print(actor.name, " | DeathState: Animation finished.")
+		animation_finished = true
 
 func exit() -> void:
-	pass
+	if enable_debug:
+		print(actor.name, " | DeathState exited.")
