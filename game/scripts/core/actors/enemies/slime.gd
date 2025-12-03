c24fbbc433e5d0efd3a9f7c4d9be5d114fa882f6
Marcus F <marcusf.dev@gmail.com>
2025-12-03 13:27:33 +0000

    Change: Refactor of Actor Scripts using BaseActor script.

---
+++ game/scripts/core/actors/enemies/slime.gd
@@ -0,0 +1,201 @@
+class_name Slime
+extends BaseActor
+
+# Script Variables
+var _prev_state_name: String = ""
+var bt_root: BTNode
+var bt: BehaviorTree
+var blackboard: Dictionary = {}
+
+func get_blackboard() -> Dictionary:
+	return blackboard
+
+# ===============================
+# ===== Actor Intialization =====
+# ===============================
+
+func _ready() -> void:
+	setup_states()
+	setup_blackboard()
+	setup_behavior_tree()
+	setup_signals()
+
+func setup_blackboard() -> void:
+	blackboard["fsm"] = state_machine
+	
+	blackboard["is_grounded"] = true
+	blackboard["has_collided"] = false
+	
+	blackboard["force_idle"] = false
+	blackboard["force_jump"] = false
+	blackboard["force_patrol"] = false
+	blackboard["force_heal"] = false
+	blackboard["force_injure"] = false
+	
+	blackboard["can_patrol"] = true
+	blackboard["can_idle"] = true
+	blackboard["can_jump"] = true
+	blackboard["can_die"] = false
+	
+	blackboard["locked"] = false
+	blackboard["move_direction"] = 1
+	blackboard["intent"] = "Fall" # Note: Must Match StateMachine Starting State
+
+func setup_behavior_tree() -> void:
+	bt_root = BTSelector.new([
+		BTSequence.new([
+			BTCondition.new("is_grounded", false),
+			BTCondition.new("locked", false),
+			BTAction.new("Fall")
+		]),
+		BTSequence.new([
+			BTCondition.new("is_grounded", true),
+			BTCondition.new("force_idle", true),
+			BTCondition.new("locked", false),
+			BTAction.new("Idle")
+		]),
+		BTSequence.new([
+			BTCondition.new("is_grounded", true),
+			BTCondition.new("force_jump", true),
+			BTCondition.new("locked", false),
+			BTAction.new("Jump")
+		]),
+		BTSequence.new([
+			BTCondition.new("is_grounded", true),
+			BTCondition.new("force_patrol", true),
+			BTCondition.new("locked", false),
+			BTAction.new("Move")
+		]),
+		BTSequence.new([
+			BTCondition.new("force_heal", true),
+			BTCondition.new("locked", false),
+			BTAction.new("Heal")
+		]),
+		BTSequence.new([
+			BTCondition.new("force_injure", true),
+			BTCondition.new("locked", false),
+			BTAction.new("Injured")
+		]),
+		BTSequence.new([
+			BTCondition.new("can_die", true),
+			BTAction.new("Death")
+		]),
+		BTSequence.new([
+			BTCondition.new("is_grounded", true),
+			BTCondition.new("has_collided", false),
+			BTCondition.new("can_patrol", true),
+			BTCondition.new("locked", false),
+			BTAction.new("Move")
+		]),
+		BTSequence.new([
+			BTCondition.new("is_grounded", true),
+			BTCondition.new("has_collided", true),
+			BTCondition.new("locked", false),
+			BTSelector.new([
+				BTSequence.new([
+					BTCondition.new("can_jump", true),
+					BTCondition.new("random_chance", 0.50),
+					BTCondition.new("locked", false),
+					BTAction.new("Jump"),
+				]),
+				BTSequence.new([
+					BTCondition.new("can_idle", true),
+					BTCondition.new("locked", false),
+					BTAction.new("Idle")
+				])
+			])
+		]),
+	])
+	bt = BehaviorTree.new(bt_root, blackboard, self)
+
+func setup_signals() -> void:
+	pickup_permission_comp.pickup_received.connect(pickup_signal_receiver)
+	hurtbox.hit_received.connect(hit_signal_receiver)
+	health_comp.health_empty.connect(death_signal_receiver)
+	SignalBus.actor_died.connect(destroy_actor)
+
+func setup_states() -> void:
+	state_machine.init(self, animations, blackboard)
+	_idle_state()
+	_patrol_state()
+	_jump_state()
+	_fall_state()
+	_heal_state()
+	_injured_state()
+	_death_state()
+
+# ==============================
+# ===== Actor State Setup ======
+# ==============================
+
+func _idle_state() -> void:
+	pass
+
+func _patrol_state() -> void:
+	pass
+
+func _jump_state() -> void:
+	pass
+
+func _fall_state() -> void:
+	pass
+
+func _heal_state() -> void:
+	pass
+
+func _injured_state() -> void:
+	pass
+
+func _death_state() -> void:
+	pass
+
+# ==============================
+# ====== Actor Physics =======
+# ==============================
+
+func _physics_process(delta: float) -> void:
+	gravity_comp.apply_physics(delta)
+	ground_check_comp.apply(delta)
+
+	var grounded : bool = ground_check_comp.is_grounded
+	blackboard["is_grounded"] = grounded
+	
+	animation_comp.animation_flip(delta)
+	jump_comp.update_timer(delta)
+	state_machine.process_physics(delta)
+	
+	actor.move_and_slide()
+	ground_check_comp.post_update()
+
+func _process(delta: float) -> void:
+	bt.tick()
+	state_machine.process_frame(delta)
+	
+	if enable_debug:
+		var current_state_name : Variant = state_machine.current_state.name
+		if current_state_name != _prev_state_name:
+			_prev_state_name = current_state_name
+			print("==============================")
+			print("[STATE CHANGE] →", current_state_name)
+			for key: Variant in blackboard.keys():
+				print(" ", key, ":", blackboard[key])
+			print("==============================")
+
+# ===============================
+# === Actor Signal Receivers ====
+# ===============================
+
+func death_signal_receiver() -> void:
+	blackboard["can_die"] = true
+
+func pickup_signal_receiver(data: Variant) -> void:
+	if data["item_type"] == "health":
+		blackboard["pickup_data"] = data["item_value"]
+		blackboard["force_heal"] = true
+		blackboard["locked"] = false
+
+func hit_signal_receiver(hurtbox_owner: Node, hitbox_data: Variant) -> void:
+	if hurtbox_owner == actor:
+		blackboard["hitbox_data"] = hitbox_data
+		blackboard["force_injure"] = true
+		blackboard["locked"] = false
--- game/scripts/core/actors/player/player.gd
+++ game/scripts/core/actors/player/player.gd
@@ -1,87 +1,31 @@
 class_name Player
-extends CharacterBody2D
+extends BaseActor
 
-## Assign the parent entity to the script.
-@export var actor_path: NodePath
-## Enables debug messages in the output terminal. [br]
-## [b]Note:[/b] Useful for development and troubleshooting.
-@export var enable_debug: bool = false
+# ===============================
+# ===== Actor Intialization =====
+# ===============================
 
-@export_category("Actor Settings")
-@export var animations_path: NodePath
-
-@export_group("Temporary Options")
-@export var damage: int = 1
-
-@export_group("State Paths")
-@export var state_machine_path: NodePath
-@export var idle_state_path: NodePath
-@export var move_state_path: NodePath
-@export var jump_state_path: NodePath
-@export var fall_state_path: NodePath
-@export var attack_state_path: NodePath
-@export var injured_state_path: NodePath
-@export var heal_state_path: NodePath
-@export var death_state_path: NodePath
-
-@export_group("Component Paths")
-@export var ground_check_component: NodePath
-@export var animation_component: NodePath
-@export var movement_component: NodePath
-@export var jump_component: NodePath
-@export var gravity_component: NodePath
-@export var pickup_permission_component: NodePath
-@export var health_component: NodePath
-@export var hurtbox_component: NodePath
-@export var hitbox_component: NodePath
-
-@onready var actor: CharacterBody2D = get_node_or_null(actor_path)
-@onready var animations: AnimatedSprite2D = get_node_or_null(animations_path)
-	
-@onready var state_machine: Node = get_node_or_null(state_machine_path)
-@onready var idle_state: Node = get_node_or_null(idle_state_path)
-@onready var move_state: Node = get_node_or_null(move_state_path)
-@onready var jump_state: Node = get_node_or_null(jump_state_path)
-@onready var fall_state: Node = get_node_or_null(fall_state_path)
-@onready var attack_state: Node = get_node_or_null(attack_state_path)
-@onready var injured_state: Node = get_node_or_null(injured_state_path)
-@onready var heal_state: Node = get_node_or_null(heal_state_path)
-@onready var death_state: Node = get_node_or_null(death_state_path)
-	
-@onready var ground_check_comp: Node = get_node_or_null(ground_check_component)
-@onready var animation_comp: Node = get_node_or_null(animation_component)
-@onready var movement_comp: Node = get_node_or_null(movement_component)
-@onready var jump_comp: Node = get_node_or_null(jump_component)
-@onready var gravity_comp: Node = get_node_or_null(gravity_component)
-@onready var pickup_permission_comp: Node = get_node_or_null(pickup_permission_component)
-@onready var hurtbox_comp: Area2D = get_node_or_null(hurtbox_component)
-@onready var hitbox_comp: Area2D = get_node_or_null(hitbox_component)
-@onready var health_comp: Node = get_node_or_null(health_component)
-
-# ==============================
-# ===== Intialization =====
-# ==============================
 func _ready() -> void:
-	_setup_states()
-	_setup_signals()
+	setup_states()
+	setup_signals()
+
+func setup_signals() -> void:
+	pickup_permission_comp.pickup_received.connect(pickup_signal_receiver)
+	hurtbox.hit_received.connect(hit_signal_receiver)
+	health_comp.health_empty.connect(death_signal_receiver)
+
+func setup_states() -> void:
 	state_machine.init(self, animations)
-
-func _setup_signals() -> void:
-	pickup_permission_comp.pickup_received.connect(pickup_received)
-	hurtbox_comp.hit_received.connect(hit_received)
-	health_comp.health_empty.connect(death_received)
-
-# ==============================
-# ===== Player State Setup =====
-# ==============================
-
-func _setup_states() -> void:
 	_idle_state()
 	_move_state()
 	_jump_state()
 	_fall_state()
 	_heal_state()
 	_injured_state()
+
+# ==============================
+# ===== Actor State Setup ======
+# ==============================
 
 func _idle_state() -> void:
 	idle_state.handle_physics = func(_delta: float) -> State:
@@ -205,7 +149,13 @@ func _injured_state() -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	state_machine.process_input(event)
-	trigger_attack()
+	
+	if InputManagerClass.is_attack_pressed():
+		state_machine.change_state(attack_state)
+
+# ==============================
+# ====== Actor Physics =======
+# ==============================
 
 func _physics_process(delta: float) -> void:
 	ground_check_comp.apply(delta)
@@ -220,7 +170,14 @@ func _physics_process(delta: float) -> void:
 	actor.move_and_slide() 
 	ground_check_comp.post_update()
 
-func pickup_received(data: Variant) -> void:
+func _process(delta: float) -> void:
+	state_machine.process_frame(delta)
+
+# ===============================
+# === Actor Signal Receivers ====
+# ===============================
+
+func pickup_signal_receiver(data: Variant) -> void:
 	if data["item_type"] == "health":
 		heal_state.set_heal_data(data)
 		state_machine.change_state(heal_state)
@@ -228,18 +185,10 @@ func pickup_received(data: Variant) -> void:
 		if enable_debug:
 			print(actor.name, " | Congratulations you collected ",data["item_value"]," coin!")
 
-func hit_received(hurtbox_owner: Node, hitbox_data: Variant) -> void:
+func hit_signal_receiver(hurtbox_owner: Node, hitbox_data: Variant) -> void:
 	if hurtbox_owner == actor:
 		injured_state.set_injured_data(hitbox_data)
 		state_machine.change_state(injured_state)
 
-func death_received() -> void:
+func death_signal_receiver() -> void:
 	state_machine.change_state(death_state)
-
-func _process(delta: float) -> void:
-	state_machine.process_frame(delta)
-
-func trigger_attack() -> void:
-	if InputManagerClass.is_attack_pressed():
-		if attack_state:
-			state_machine.change_state(attack_state)
