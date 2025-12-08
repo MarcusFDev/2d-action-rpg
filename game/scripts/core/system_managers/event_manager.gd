9f687cd3fc4fea97370c7170c74e92e701e28a6f
Marcus F <marcusf.dev@gmail.com>
2025-12-08 17:16:27 +0000

    Change: Renamed SignalBus to EventManager and removed as Autoload.

---
+++ game/scripts/core/system_managers/event_manager.gd
@@ -0,0 +1,17 @@
+@icon("uid://dafrs6n457wu")
+class_name EventManager
+extends Node
+
+@warning_ignore("unused_signal")
+signal actor_died(actor: Node)
+
+## Enables debug messages in the output terminal. [br]
+## [b]Note:[/b] Useful for development and troubleshooting.
+@export var enable_debug: bool = false
+
+# ===============================
+# ==== Manager Intialization ====
+# ===============================
+
+func _enter_tree() -> void:
+	Global.event_manager = self
