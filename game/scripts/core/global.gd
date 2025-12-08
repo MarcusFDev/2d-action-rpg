eea7a3304a96b14ef3ef68467933954315debd4a
Marcus F <marcusf.dev@gmail.com>
2025-12-08 13:36:19 +0000

    Add: Global Autoload script & removed Game/UiManager as autoloads.

---
+++ game/scripts/core/global.gd
@@ -0,0 +1,11 @@
+extends Node
+
+# =========================
+# ===== Global Script =====
+# =========================
+
+## DEVELOPER NOTE: This autoload is used for scripts to self-assign themselves globablly
+## on their own scene loads. This avoids common autoload complications & drawbacks. 
+
+var ui_manager: Node
+var game_manager: Node
